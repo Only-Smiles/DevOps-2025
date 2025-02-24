c6ddfb7b0bea24ce02ccc604e22748c366173c6e
DumbDane <lrpe@itu.dk>
2025-02-24 14:37:31 +0100

    fix(latest): Use params[] instead of request.env and fix variable return messages

--- src/minitwit/api/routes/fllws.rb
+++ src/minitwit/api/routes/fllws.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 class MiniTwit < Sinatra::Base
 
-  post '/fllws/:username' do 
+  post '/fllws/:username' do
     content_type :json
     req = req_from_sim(request)
     return req unless req.nil?
@@ -13,20 +13,20 @@ class MiniTwit < Sinatra::Base
     who_id = get_user_id(who_username)
     whom_username = @data['follow'] || @data['unfollow']
     whom_id = get_user_id(whom_username)
-    if whom_id.nil? || who_id.nil?
+    if who_id.nil? || whom_id.nil?
       status 404
-      body JSON({ 'message': "Either #{who_id} or #{@data} not found" })
+      body JSON({ 'message': "User not found" })
     end
 
       
     if @data.key?('unfollow')
       query_db('DELETE FROM follower WHERE who_id = ? AND whom_id = ?', [who_id, whom_id])
       status 200
-      body JSON({ 'message': 'Unfollowed #{whom_id}' })
+      body JSON({ 'message': 'Unfollowed ' + whom_username })
     elsif @data.key?('follow')
       query_db('INSERT INTO follower (who_id, whom_id) VALUES (?, ?)', [who_id, whom_id])
       status 200
-      body JSON({ 'message': 'Followed #{whom_id}',  })
+      body JSON({ 'message': 'Followed ' + whom_username,  })
     end
   end
 
@@ -35,7 +35,7 @@ class MiniTwit < Sinatra::Base
     req = req_from_sim(request)
     return req unless req.nil?
 
-    no_followers = request.env['no'] || 100
+    no_followers = params[:no] || 100
     username = params[:username]
     id = get_user_id(username)
 
