194fc53a8037eca5e2c5bbc558232326ee5c6abe
DumbDane <lrpe@itu.dk>
2025-02-28 18:06:22 +0100

    fix: missing end of class

--- src/minitwit/controllers/base_controller.rb
+++ src/minitwit/controllers/base_controller.rb
@@ -4,8 +4,10 @@ class BaseController < Sinatra::Base
     set :views, File.join(__dir__, '..', 'views')
     set :public_folder, File.join(__dir__, '..', 'public')
   end
-
   
   helpers DbHelper
   helpers AuthHelper
   helpers FormatHelper  
+
+end
+
