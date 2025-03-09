6e8ec7434d772733e91b13541d668d05c14d8cf0
DumbDane <lrpe@itu.dk>
2025-03-09 17:29:40 +0100

    feat: Prepare for database migration in the cloud

--- remote_files/migration.sh
+++ remote_files/migration.sh
@@ -3,9 +3,9 @@
 docker run --rm -it \
 --network minitwit_main \
 --env PGPASSWORD=${DB_PWD} \
---volume ./src/minitwit/tmp/minitwit.db:/tmp/minitwit.db:ro \
+--volume /tmp/minitwit.db:/tmp/minitwit.db:ro \
 --volume ./load.db:/tmp/load.db \
 --platform linux/amd64 \
 dimitri/pgloader bash -c "pgloader /tmp/load.db" \
 && export POSTGRES=yes \
-&& docker compose -f dev-compose.yml up --build -d --force-recreate
+&& docker compose up -d --force-recreate
